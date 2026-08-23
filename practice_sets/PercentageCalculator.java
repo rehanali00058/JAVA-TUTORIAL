431c086323e26faeb023f421322f809bbb16360b
Rehan Ali <rehanali00058@gmail.com>
2026-08-24 01:08:31 +0530

    practice set 1

---
+++ practice_sets/PercentageCalculator.java
@@ -0,0 +1,28 @@
+package practice_sets.practice_set_1;
+
+import java.util.Scanner;
+
+public class PercentageCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of Science subject:");
+        float a = sc.nextFloat();
+        System.out.println("Enter number of Maths subject:");
+        float b = sc.nextFloat();
+        System.out.println("Enter number of Hindi subject:");
+        float c = sc.nextFloat();
+        System.out.println("Enter number of CS subject:");
+        float d = sc.nextFloat();
+        System.out.println("Enter number of English subject:");
+        float e = sc.nextFloat();
+
+        float percentage = (a+b+c+d+e)*100f/500f;
+        System.out.println("your percentage is"+percentage);
+
+        float SGPA = percentage/10;
+        System.out.println("the sgpa is"+SGPA);
+
+
+
+    }
+}
