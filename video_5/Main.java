70e0d6be3ce8fc00b791f45feaeddfd07ce7bb93
Rehan Ali <rehanali00058@gmail.com>
2026-08-23 21:16:01 +0530

    Updated video 5 Main

--- video_5/Main.java
+++ video_5/Main.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("It works!");
+        System.out.println("Taking Input From the User");
         Scanner sc = new Scanner(System.in); // system.in hepls in reading from keyboard
-        System.out.println("enter number 1");
-        int a = sc.nextInt();
-        System.out.println("enter number 2");
-        int b = sc.nextInt();
-        int sum = a+b;
-        System.out.println("the sum of these numbers is");
-        System.out.println(sum);
+        // System.out.println("enter number 1 :");
+        // int a = sc.nextInt();
+        // boolean b1 = sc.hasNextInt();
+        // System.out.println(b1);
+        String str = sc.nextLine(); //if nextline() is not used it will read only one 
+        System.out.println(str);
+
+        // System.out.println("Enter number 1 :");
+        // float a = sc.nextFloat();
+        // System.out.println("enter number 2 :");
+        // int b = sc.nextInt();
+        // float b = sc.nextFloat();
+        // float sum = a+b;
+        // System.out.println("the sum of these numbers is");
+        // System.out.println(sum);
 
         
     }
