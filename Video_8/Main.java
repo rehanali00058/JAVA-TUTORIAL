afa75ee4343d811fc572f71f974e6d5b049aab3e
Rehan Ali <rehanali00058@gmail.com>
2026-08-26 00:24:26 +0530

    Refactor Main.java to include examples of assignment, comparison, logical, and bitwise operators

--- Video_8/Main.java
+++ Video_8/Main.java
@@ -1,7 +1,27 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        // assignment operators
+        int a = 4;
+        // int b = 6%a; //module operator
+        // 4.8%1.1 --> returns decimal remainder
+        // 2 assignment operators
+        int b = 9;
+        b*=3; // *=,-=,
+        System.out.println(b);
+        System.out.println(6==8);
+        System.out.println(6==6); 
+        // 3 comparison operators
+        System.out.println(66>6);
+        System.out.println(66<6);
+        // 4 logical operators
+        System.out.println(66<6 && 64>5 );
+        System.out.println(66>6 && 64>5 );
+        System.out.println(66>6 || 64>5 );
+        System.out.println(66<6 || 64>5 );
+        // 5 bitwise operators
+        System.out.println(2&3);
+
         
 
     }
