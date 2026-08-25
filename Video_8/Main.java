efeb74a04d437fbcf4f929bc01b117b909985db4
Rehan Ali <rehanali00058@gmail.com>
2026-08-26 00:33:52 +0530

    updated main.java

--- Video_8/Main.java
+++ Video_8/Main.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // assignment operators
         int a = 4;
+        System.out.println(a);
         // int b = 6%a; //module operator
         // 4.8%1.1 --> returns decimal remainder
         // 2 assignment operators
@@ -15,9 +15,9 @@ public class Main {
         System.out.println(66>6);
         System.out.println(66<6);
         // 4 logical operators
-        System.out.println(66<6 && 64>5 );
+        System.out.println(66<6 );
         System.out.println(66>6 && 64>5 );
-        System.out.println(66>6 || 64>5 );
+        System.out.println(66>6 );
         System.out.println(66<6 || 64>5 );
         // 5 bitwise operators
         System.out.println(2&3);
