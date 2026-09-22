d482a1fd37e040f9d880f258c57b02d36d4dc600
Rehan Ali <rehanali00058@gmail.com>
2026-09-22 22:42:19 +0530

    Associativity and precedence

--- Video_9/Main.java
+++ Video_9/Main.java
@@ -1,7 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        
+        //Precedence_and_Associativity
+        // int a = 6*5-34/2;
+        // int b = 6/5-34*2;
+        // System.out.println(a);
+        // System.out.println(b);
+        int x = 6;
+        int y = 1;
+        int z = x*y/2;
+        System.out.println(z);
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int k = b*b-(4*a*c)/(2*a);
+        System.out.println(k); 
 
-        
+
+
+
+
     }    
 }
