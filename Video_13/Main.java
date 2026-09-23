c47e6a5a31fef646adf926bb72ab1011656319df
Rehan Ali <rehanali00058@gmail.com>
2026-09-23 15:28:24 +0530

    modified main for video 13

--- Video_13/Main.java
+++ Video_13/Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String name = new String("Rehan");
-        String surname = "Ali";
+        // String name = new String("Rehan");
+        // String surname = "Ali";
         // System.out.println(name);
         // System.out.println(surname);
         // System.out.print(name+" "+surname);// will not print a new line at the end just prints the give arguement
@@ -10,8 +10,8 @@ public class Main {
         float b = 67.98f;
         System.out.printf("the value of a   is %d and the value of b is %8.2f",a,b);//prints the value of a and b in format
         // sout.printf will behave same as printf // the floar pointer can be formatted using numbers in front of the %f pointer
-        Scanner sc = new Scanner(System.in);
-        String st = sc.next();
+        // Scanner sc = new Scanner(System.in);
+        // String st = sc.next();
         // System.out.println(st);
         // System.out.println();
 
